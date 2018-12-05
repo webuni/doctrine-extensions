38fa67b982a29a408703313aade1af7b7c3d3957
Martin Hasoň <martin.hason@gmail.com>
2018-12-05 11:47:05 +0100

    Add subscriber to convert mapping from array type to json type

---
+++ src/Mapping/JsonTypeSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This is part of the webuni/doctrine-extensions package.
+ *
+ * (c) Martin Hasoň <martin.hason@gmail.com>
+ * (c) Webuni s.r.o. <info@webuni.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webuni\DoctrineExtensions\Mapping;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Events;
+
+final class JsonTypeSubscriber implements EventSubscriber
+{
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::loadClassMetadata,
+        ];
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
+    {
+        $metadata = $eventArgs->getClassMetadata();
+
+        foreach ($metadata->fieldMappings as $field => $mapping) {
+            if (isset($mapping['type']) && \in_array($mapping['type'], [Type::TARRAY, Type::JSON_ARRAY], true)) {
+                $metadata->fieldMappings[$field]['type'] = Type::JSON;
+            }
+        }
+    }
+}
