c7e88911654a3be412c608fb0769335b2737022f
Martin Hasoň <martin.hason@gmail.com>
2020-09-02 13:33:42 +0200

    Add universal STRING function

---
+++ src/Query/Functions/StringFunction.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This is part of the webuni/doctrine-extensions package.
+ *
+ * (c) Martin Hasoň <martin.hason@gmail.com>
+ * (c) Webuni s.r.o. <info@webuni.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Webuni\DoctrineExtensions\Query\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * Usage : StringFunction STRING()
+ */
+final class StringFunction extends FunctionNode
+{
+    public $select;
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return '('.$sqlWalker->walkStringPrimary($this->select).')';
+    }
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->select = $parser->StringExpression();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
